dba43aea5c4370c164d2926ac6135ee646dde85c
Shahwaiz Hassan <shahwaizhassan9@gmail.com>
2024-04-03 13:32:41 +0500

    Done paper

---
+++ FA21-BCS-126-A_MIDLABEXAM/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const imgElements = document.querySelectorAll("img");
+
+    imgElements.forEach(function(imgElement) {
+        imgElement.addEventListener("mouseover", function () {
+            const imageName = imgElement.alt;
+            const imgNameElement = document.getElementById("text1");
+            imgNameElement.textContent = imageName;
+        });
+
+        imgElement.addEventListener("mouseout", function () {
+            const imgNameElement = document.getElementById("imgName");
+            imgNameElement.textContent = "";
+        });
+    });
+});
