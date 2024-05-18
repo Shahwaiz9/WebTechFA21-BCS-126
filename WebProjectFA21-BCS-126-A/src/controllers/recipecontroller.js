520a4f1f32d3ebf5d02e722d7056a447b51d6198
Shahwaiz Hassan <shahwaizhassan9@gmail.com>
2024-05-18 15:52:52 +0500

    data retrieved from mongo. ran env file too and fix path issues in ejs

--- WebProjectFA21-BCS-126-A/src/controllers/recipecontroller.js
+++ WebProjectFA21-BCS-126-A/src/controllers/recipecontroller.js
@@ -1,7 +1,59 @@
-const { title } = require("process");
+require('../models/database');
+const Category=require('../models/Category');
 
+
+
+const { title } = require("process");
 
 exports.homepage=async(req,res)=>{
 
-    res.render('index',{title: 'Cookbook Community'});
+    try{
+        const limitcat=5;
+        const categories=await Category.find({}).limit(limitcat);
+
+
+            
+    res.render('index',{title: 'Cookbook Community',categories});
+
+    }catch(error){
+        console.log(error)
+    }
 }
+
+// async function insertDymmyDataCategory(){
+//     try{
+//         await Category.insertMany(
+//             [
+//                 {
+//                     "name":"American",
+//                     "image":"americanfood2.jpg"
+//                 },
+//                 {
+//                     "name":"Pakistani",
+//                     "image":"pakistanfood.jpg"
+//                 },
+//                 {
+//                     "name":"Chinese",
+//                     "image":"chinesefood2.jpg"
+//                 },
+//                 {
+//                     "name":"Spanish",
+//                     "image":"spanishfood2.jpg"
+//                 },
+//                 {
+//                     "name":"Thai",
+//                     "image":"thaifood2.jpg"
+//                 },
+//                 {
+//                     "name":"Mexican",
+//                     "image":"mexicanfood2.jpg"
+//                 }
+//             ]
+
+//         );
+        
+//     }catch(error){
+//         console.log(error +' in insertdatadum')
+//     }
+// }
+//   insertDymmyDataCategory();
--- WebProjectFA21-BCS-126-A/src/main.js
+++ WebProjectFA21-BCS-126-A/src/main.js
@@ -1,18 +1,21 @@
-const express = require( 'express');
-const expressLayouts =require('express-ejs-layouts');
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+require('dotenv').config();
 
-const app=express()
-const PORT=process.env.PORT || 3000
-require('dotenv').config
-app.use(express.urlencoded({extended:true}));
-app.use(express.static('./public')) //for listing imgs like /imgs not full
-app.use(expressLayouts)
-app.set('layout','./layouts/main1.ejs')
+const app = express();
+const PORT = process.env.PORT;
 
-app.set('view engine','ejs')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public')); // for listing imgs like /imgs not full
+app.use(expressLayouts);
+app.set('layout', './layouts/main1.ejs'); 
+app.use(express.static('./views/partials'));
+app.set('view engine', 'ejs');
 
-const routes= require('./routes/reciperoutes.js')
-app.use('/',routes)
-app.listen(PORT,()=>{
-    console.log(`Listening to port ${PORT}`)
-})
+const routes = require('./routes/reciperoutes.js');
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
